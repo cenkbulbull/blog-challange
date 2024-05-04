3098451cce653df27a4ebf299141f854b2afa1a5
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-04 17:54:54 +0300

    feat: footer added

---
+++ src/components/shared/AppFooter.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="footer">
+    <div class="footer__content">
+      <div class="footer__content-info">
+        <div class="footer__content-info-about">
+          <h4 class="footer__content-info-about-title">About</h4>
+          <p class="footer__content-info-about-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </p>
+        </div>
+        <div class="footer__content-info-contact">
+          <p class="footer__content-info-contact-email">
+            Email: <span>info@jstemplate.net</span>
+          </p>
+          <p class="footer__content-info-contact-phone">
+            Phone: <span>880 123 456 789</span>
+          </p>
+        </div>
+      </div>
+      <div class="footer__content-link">
+        <div class="footer__content-link-quicklink">
+          <h4 class="footer__content-link-quicklink-title">Quick Link</h4>
+          <ul class="footer__content-link-quicklink-list">
+            <li>Home</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Archived</li>
+            <li>Author</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div class="footer__content-link-category">
+          <h4 class="footer__content-link-category-title">Category</h4>
+          <ul class="footer__content-link-category-list">
+            <li>Lifestyle</li>
+            <li>Technology</li>
+            <li>Travel</li>
+            <li>Business</li>
+            <li>Economy</li>
+            <li>Sports</li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer__content-newsletter">
+        <div class="footer__content-newsletter-heading">
+          <span>Weekly Newsletter</span>
+          <p>Get blog articles and offers via email</p>
+        </div>
+        <div class="footer__content-newsletter-form">
+          <input
+            class="footer__content-newsletter-form-input"
+            type="text"
+            placeholder="Your Email"
+          />
+          <i
+            class="fa-regular fa-envelope footer__content-newsletter-form-icon"
+          ></i>
+          <button class="footer__content-newsletter-form-button">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="footer__copyright">
+      <img
+        class="footer__copyright-logo"
+        src="@/assets/images/footerlogo.png"
+      />
+      <ul class="footer__copyright-list">
+        <li>Terms of Use</li>
+        <hr />
+        <li>Privacy Policy</li>
+        <hr />
+        <li>Cookie Policy</li>
+      </ul>
+    </div>
+  </div>
+</template>
