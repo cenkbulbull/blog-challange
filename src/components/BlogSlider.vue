f76923f2046c68e2f74756bdce70ea77fc318e99
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-05 19:32:58 +0300

    feat: blog slider added

---
+++ src/components/BlogSlider.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="slider">
+        <img class="slider__image" src="https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Zjfi6ptJ8Mmg29Ku7xOWUObe5HB8QEhmtfjp0tMUJou1nkE29BL5uax4sMisk3AIpN8qbtcYNKREdlM7-qULRbCDGPrgTElphKNBuqX60ENS41RY2xeL-B~MEPFe-t1izPEiI0z09FM4Wc6mr-DVU2l37Uhnt2tgXdTWelVpH~02Ksda-WQ3Oe8R~lKBJQrTfy5wsfZUfdM1LbBoykkBxykxd9mB4OqbMoB45GdhpZo8XP-EH1cegHLo54WS8lbmFzhPlTMwJsOD4ZGT2mQzALDgqyne-~0rvFCuoB1lD08bqMLzAO9Mim3PyJ7qykqVsYlmlaaCXyLv9WSmng8NNg__">
+        <div class="slider__content">
+            <div class="slider__content__heading">
+                <div class="slider__content__heading__badge--primary">
+                    Technology
+                </div>
+                <div class="slider__content__heading__title">
+                    <h1>The Impact of Technology on the Workplace: How Technology is Changing</h1>
+                </div>
+            </div>
+            <div class="slider__content__short-info">
+                <div class="slider__content__short-info__author">
+                    <img src="https://s3-alpha-sig.figma.com/img/f952/fa47/9a54dc57c3bfdd71f8ab78628ef9ac9a?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=esrie9sNAWLFPQ9thxqVSbIW07YaOGEiBJneAnJpaHzRkJNYUpADar-N3XpdPWp4lxYsHnYlRyQjNBQcBhKX3AX9u~9~3Bma5X9ok6Qtpvt~OEGt-XH4KBo4MOHSjyXhBI3OAesEWXxOzIDOclyqtRCf9u3lwOlHmaiJFq5Of3uphAdcIr1iTBotInX87UTb6pmqH-hkGu8M9-~zOUWBeNmp9Vuw78wPK5dImjtMJ~AKlrAtZd3KiG7LrpvC0JSP40WK8xAJz6Td5q3aBjbbJ3VFgY4dKEx0pvT11GyYYmYMeFuKx803uPKC6euJLF2wbzLgPqhMH4Jvyq0ffz1Heg__">
+                    <p>Jason Francisco</p>
+                </div>
+                <div class="slider__content__short-info__date">
+                    <p>August 20, 2022</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
