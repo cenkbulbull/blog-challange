09733c03995979fe86b1eb102ebe77a79e4bd6f2
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-06 13:50:02 +0300

    feat: data passed to blogcard component

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -2,23 +2,29 @@
   <div class="card">
     <img
       class="card__image"
-      src="https://s3-alpha-sig.figma.com/img/7465/4a8f/67369b797c8fb2e96a533fd515fb2939?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OtcB7fKeyD9MAALftBI2LJwMyPh0o~orqdQhUHy335dxjwBjB0QkAUi8T9imhHMXE~3v-c8los~Fkl5li5jxk30NViY7BToSqsZKXyJPZFo2I7ZC-NCydTOLj6kevP8X5sVbMhUv9iwpSHw~kUUpJ7UYbmhmcLekbsQCeWYKCJul1SP7UxzMf2mNQTbb3tE7JZZ5fKciqUSDl1PDsKmUpL2-lsyR8H8iGahz1UNYK8x0byjaybpcfj5-0C37bGoLTCSPv4~Tak~rGqxOhJuwnSvZnfmenRALm0Q~iFzP3wrCYqbmQIW45-uGqUy8LLJKK-Sk-~CskUwedyI1EmbEBQ__"
+      :src="blog.image"
     />
     <div class="card__content">
-      <div class="card__content-category">Technology</div>
+      <div class="card__content-category">{{blog.category}}</div>
       <h3 class="card__content-title">
-        The Impact of Technology on the Workplace: How Technology is Changing
+        {{blog.text}}
       </h3>
       <div class="card__content-footer">
         <div class="card__content-footer-author">
           <img
             class="card__content-footer-author-avatar"
-            src="https://s3-alpha-sig.figma.com/img/f952/fa47/9a54dc57c3bfdd71f8ab78628ef9ac9a?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=esrie9sNAWLFPQ9thxqVSbIW07YaOGEiBJneAnJpaHzRkJNYUpADar-N3XpdPWp4lxYsHnYlRyQjNBQcBhKX3AX9u~9~3Bma5X9ok6Qtpvt~OEGt-XH4KBo4MOHSjyXhBI3OAesEWXxOzIDOclyqtRCf9u3lwOlHmaiJFq5Of3uphAdcIr1iTBotInX87UTb6pmqH-hkGu8M9-~zOUWBeNmp9Vuw78wPK5dImjtMJ~AKlrAtZd3KiG7LrpvC0JSP40WK8xAJz6Td5q3aBjbbJ3VFgY4dKEx0pvT11GyYYmYMeFuKx803uPKC6euJLF2wbzLgPqhMH4Jvyq0ffz1Heg__"
+            :src="blog.authorImage"
           />
-          <span class="card__content-footer-author-name">Jason Francisco</span>
+          <span class="card__content-footer-author-name">{{blog.author}}</span>
         </div>
-        <span class="card__content-footer-date">August 20, 2022</span>
+        <span class="card__content-footer-date">{{blog.date}}</span>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+import {defineProps} from 'vue'
+defineProps({
+  blog: Object
+})
+</script>
