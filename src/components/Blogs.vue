111baf9921d60e34771eb353bf84a1b534ee9436
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-06 13:48:09 +0300

    feat: blogs component added

---
+++ src/components/Blogs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="blogs">
+    <div v-for="(blog, index) in data.blogs" :key="index" class="blogs__list">
+      <BlogCard :blog="blog" />
+    </div>
+  </div>
+</template>
+<script setup>
+import BlogCard from './BlogCard.vue'
+import data from '@/data/fakeData.json'
+</script>
