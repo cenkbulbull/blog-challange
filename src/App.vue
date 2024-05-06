5fab4ef667ebf3f1638a73ac61d4ff293318c83f
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-06 18:38:27 +0300

    feat: home page added

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,12 @@
-<template>test</template>
+<template>
+  <div class="container">
+    <AppNavbar />
+    <router-view />
+  </div>
+  <AppFooter />
+</template>
+<script setup>
+import AppNavbar from './components/shared/AppNavbar.vue'
+import AppFooter from './components/shared/AppFooter.vue'
+</script>
 <style lang="scss"></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="home">
+        <BlogSlider />
+        <div class="home__content">
+            <AdsCard />
+            <div class="home__content__blogs">
+                <div class="home__content__blogs__heading">
+                    <h3>Latest Post</h3>
+                </div>
+                <div class="home__content__blogs__layout">
+                    <Blogs/>
+                </div>
+                <button class="home__content__blogs__button">View All Post</button>
+            </div>
+            <AdsCard />
+        </div>
+    </div>
+</template>
+<script setup>
+import Blogs from '../components/Blogs'
+import AdsCard from '../components/AdsCard'
+import BlogSlider from '../components/BlogSlider'
+
+</script>
+<style lang="scss">
+.home{
+    display: flex;
+    flex-direction: column;
+    gap:80px;
+    &__content{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap:32px;
+        &__blogs{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 32px;
+            &__heading{
+                width: 100%;
+                font-weight: 700;
+                padding-left: 14px;
+                color: #181A2A;
+            }
+            &__button{
+                font-family: "Work Sans", sans-serif;
+                font-size: 15px;
+                font-weight: 500;
+                line-height: 24px;
+                word-break: break-word;
+                width: 142px;
+                height: 48px;
+                padding: 12px 20px 12px 20px;
+                border-radius: 6px;
+                border: 1px solid rgba(105, 106, 117, 0.3);
+                color: #696A75;
+                background-color: #ffffff;
+                cursor: pointer;
+                &:hover{
+                    background-color: rgba(105, 106, 117, 0.3);
+                    border: 1px solid #ffffff;
+                    color: #ffffff;
+                    transition: all .2s;
+                }
+            }
+        }
+    }
+}
+</style>
