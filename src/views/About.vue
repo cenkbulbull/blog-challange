e2c3b1f2367f96c0b226b818867c503d9b6684d6
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-06 18:55:12 +0300

    feat: about page added

---
+++ src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="about">
+        <AuthorInfoCard/>
+        <div class="about__blogs">
+            <div class="about__blogs__heading">
+                <h3>Latest Post</h3>
+            </div>
+            <Blogs/>
+        </div>
+    </div>
+</template>
+<script setup>
+import AuthorInfoCard from '@/components/AuthorInfoCard.vue';
+import Blogs from '@/components/Blogs.vue';
+</script>
+<style lang="scss">
+.about{
+    display: flex;
+    flex-direction: column;
+    gap:48px;
+    &__blogs{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap:32px;
+        &__heading{
+            width: 100%;
+            font-weight: 700;
+            padding-left: 14px;
+            color: #181A2A;
+        }
+    }
+}
+</style>
