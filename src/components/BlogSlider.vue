b9fa20a4a9ed629bac05655c9ea1d5d33bd310da
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-07 21:55:24 +0300

    feat: autoplay feature added to slider

--- src/components/BlogSlider.vue
+++ src/components/BlogSlider.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slider">
-    <swiper-container  navigation="true">
+    <swiper-container  navigation="true" :autoplay="{delay: 2500}">
       <swiper-slide v-for="(blog, index) in data.blogs" :key="index">
         <img class="slider__image" :src="blog.image" />
         <BlogInfo :blog="blog" class="slider__info" />
