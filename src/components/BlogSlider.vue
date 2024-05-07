59cd7bf9069b7e520d7edaf2f6d86e1d2d03b7a9
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-07 21:50:02 +0300

    feat: created carousel using swiperjs

--- src/components/BlogSlider.vue
+++ src/components/BlogSlider.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="slider">
-        <img class="slider__image" src="https://s3-alpha-sig.figma.com/img/eb4f/aad2/4394e91108e011b0d07581596959713b?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Zjfi6ptJ8Mmg29Ku7xOWUObe5HB8QEhmtfjp0tMUJou1nkE29BL5uax4sMisk3AIpN8qbtcYNKREdlM7-qULRbCDGPrgTElphKNBuqX60ENS41RY2xeL-B~MEPFe-t1izPEiI0z09FM4Wc6mr-DVU2l37Uhnt2tgXdTWelVpH~02Ksda-WQ3Oe8R~lKBJQrTfy5wsfZUfdM1LbBoykkBxykxd9mB4OqbMoB45GdhpZo8XP-EH1cegHLo54WS8lbmFzhPlTMwJsOD4ZGT2mQzALDgqyne-~0rvFCuoB1lD08bqMLzAO9Mim3PyJ7qykqVsYlmlaaCXyLv9WSmng8NNg__">
-        <div class="slider__content">
-            <BlogInfo class="slider__info"/>
-        </div>
-    </div>
+  <div class="slider">
+    <swiper-container  navigation="true">
+      <swiper-slide v-for="(blog, index) in data.blogs" :key="index">
+        <img class="slider__image" :src="blog.image" />
+        <BlogInfo :blog="blog" class="slider__info" />
+      </swiper-slide>
+    </swiper-container>
+  </div>
 </template>
 <script setup>
-import BlogInfo from './BlogInfo.vue';
+import BlogInfo from './BlogInfo.vue'
+import data from '@/data/fakeData.json'
 </script>
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/css/style.scss'
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
 createApp(App).use(store).use(router).mount('#app')
