4bedf89bd3946b322b5272eeaf0f310e172d7337
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-07 14:37:20 +0300

    feat: breadcrumb component added

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="breadcrumb">
+        <ul class="breadcrumb__list">
+            <li class="breadcrumb__list__link">
+                Home
+            </li>
+            <li class="breadcrumb__list__link breadcrumb__list__link--active">
+                Blog Listing
+            </li>
+        </ul>
+    </div>
+</template>
